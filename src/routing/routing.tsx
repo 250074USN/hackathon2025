d2143bdda807659a6504e88a5dfd9aea8f7e7eb8
JoakimTronrud <250074@usn.no>
2025-03-29 14:38:19 +0100

    Lagde pages og fikset routing

--- src/routing/routing.tsx
+++ src/routing/routing.tsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import FinnSenter from "../pages/FinnSenter";
+import Tjenester from "../pages/Tjenester";
+import Timeplan from "../pages/Timeplan";
+import BliMedlem from "../pages/BliMedlem";
+import Kontakt from "../pages/KontaktOss";
 
 function Routing() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="*" element={<HomePage />} />
+            <Route path="/finn-senter" element={<FinnSenter />} />
+            <Route path="/tjenester" element={<Tjenester />} />
+            <Route path="/timeplan" element={<Timeplan />} />
+            <Route path="/bli-medlem" element={<BliMedlem />} />
+            <Route path="/kontakt" element={<Kontakt />} />
+            <Route path="*" element={<HomePage />} /> {/* Fallback route */}
         </Routes>
     );
 }
